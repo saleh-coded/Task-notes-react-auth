6d2a8d45a6b16720997ad36f8ccf00785bb78b69
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2024-09-01 13:55:47 +0300

    Updated Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Notes from "./pages/Notes";
-import Note from "./pages/Note";
-import Users from "./pages/Users";
-import { clear } from "@testing-library/user-event/dist/clear";
 
 function App() {
   return (
     <div className="App font-mono ">
       <Navbar />
-      <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/notes" Component={Notes} />
-        <Route path="/notes/:noteId" Component={Note} />
-        <Route path="/login" Component={Login} />
-        <Route path="/register" Component={Register} />
-        <Route path="/users" Component={Users} />
-      </Routes>
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,18 +3,53 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Notes from "./pages/Notes";
+import Note from "./pages/Note";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import User from "./pages/Users";
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/notes",
+        element: <Notes />,
+      },
+      {
+        path: "/notes/:noteId",
+        element: <Note />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/users",
+        element: <User />,
+      },
+    ],
+  },
+]);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
